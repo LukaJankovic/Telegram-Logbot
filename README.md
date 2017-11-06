1abc28d719ce142ce7ca0314d61df300d896e482
Luka Jankovic <luka@LServer.localhost>
2017-11-06 22:44:58 +0100

    re-structured the project

--- README.md
+++ README.md
@@ -1,7 +1,48 @@
 ## Logbot for Telegram
 
-Sends you a message when an incorrect login was made, along with a location approximation of the IP that was trying to log in. Tested on FreeBSD. Might, or might not work on other systems.
+Sends you a message when an incorrect login was made, along with a location approximation of the IP that was trying to log in. Tested on FreeBSD. Might, or might not work on other systems, but open an issue if you need help setting it up on your system!
 
-## No...
+## Requirements
 
-Look, I know, ok? The source code is beyond cringy and stupid. But I think that as a soceity in 2017 we should not judge and accept that some people are shit developers. (Probably just me but still...)
+* Python3
+* [python-telegram-bot](https://github.com/python-telegram-bot/python-telegram-bot)
+* tailer
+
+## Usage
+
+`python3 logbot.py $token $chat_id`
+
+The token can be acquired from the Botfather.
+
+The chat id can be obtained by following [this guide](https://github.com/python-telegram-bot/python-telegram-bot/wiki/Extensions-%E2%80%93-Your-first-Bot), until they mention the `def start(bot, update):` function. From there the chat_id can be acquired using `update.message.chat_id`
+
+## Autostart
+
+Create a service that runs the script, and enable it on boot. This will be different depending on which OS you're using. I'm running FreeBSD, so for me this file in rc.d works fine:
+
+```
+
+#!/bin/sh
+
+# PROVIDE: logbot
+
+. /etc/rc.subr
+
+name="logbot"
+rcvar=`set_rcvar`
+start_cmd="logbot_start"
+
+logbot_start()
+{
+    /usr/local/bin/python3 /usr/home/luka/Documents/Python/Logbot/logbot.py $token $chatid &
+}
+
+load_rc_config $name
+run_rc_command "$1"
+
+```
+
+## Plans
+
+* Send notification when user logs in
+* Some kind of config system? Also, incorporate being able to acquire chat_id.
--- logbot.py
+++ logbot.py
@@ -5,26 +5,15 @@ import tailer
 import re
 import urllib.request
 import json
+import sys
 
-chatid = 
+chatid = sys.argv[2]
 
-bot = telegram.Bot(token="")
+bot = telegram.Bot(token=sys.argv[1])
 
-for line in tailer.follow(open("/var/log/auth.log")):
-    if line.split()[5] == "error:":
-        ip = ""
-        if line.split()[10] == "illegal":
-            bot.send_message(chat_id=chatid, text="Authentication error for user "+line.split()[12]+" from "+line.split()[14])
-            ip = line.split()[14]
-        elif line.split()[6] == "maximum":
-             bot.send_message(chat_id=chatid, text="Authentication error for user "+line.split()[11]+" from "+line.split()[13])
-             ip = line.split()[13]
-        else:
-            bot.send_message(chat_id=chatid, text="Authentication error for user "+line.split()[10]+" from "+line.split()[12])
-            ip = line.split()[12]
-            
-        if not re.search("\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}", ip):
-            ip = ip.split(".")[0].replace("-", ".")
+def sendIPOnMap(ip):
+    if not re.search("\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}", ip):
+        ip = ip.split(".")[0].replace("-", ".")
 
         ip = re.sub("[a-zA-Z]", "", ip)
 
@@ -35,3 +24,22 @@ for line in tailer.follow(open("/var/log/auth.log")):
                     bot.send_location(chat_id=chatid, latitude=data["latitude"], longitude=data["longitude"])
             except HTTPError as e:
                 print(e)
+
+for line in tailer.follow(open("/var/log/auth.log")):
+
+    ip = ""
+
+    if line.split()[5] == "error:":
+        if line.split()[10] == "illegal":
+            bot.send_message(chat_id=chatid, text="Authentication error for user "+line.split()[12]+" from "+line.split()[14])
+            sendIPOnMap(line.split()[14])
+        elif line.split()[6] == "maximum":
+             bot.send_message(chat_id=chatid, text="Authentication error for user "+line.split()[11]+" from "+line.split()[13])
+             sendIPOnMap(line.split()[13])
+        else:
+            bot.send_message(chat_id=chatid, text="Authentication error for user "+line.split()[10]+" from "+line.split()[12])
+            sendIPOnMap(line.split()[12])
+
+    if line.split()[5] == "Failed":
+         bot.send_message(chat_id=chatid, text="Authentication error for user "+line.split()[8]+" from "+line.split()[10])
+         sendIPOnMap(line.split()[10])
